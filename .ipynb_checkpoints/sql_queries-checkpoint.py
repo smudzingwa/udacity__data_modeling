e4d67e0fbdc830a7fd4d8fdde705beabe084e30a
simba <simudzingwa@live.com>
2022-10-21 11:02:00 +0000

    UPDATE the submission

--- .ipynb_checkpoints/sql_queries-checkpoint.py
+++ .ipynb_checkpoints/sql_queries-checkpoint.py
@@ -36,7 +36,7 @@ songplay_table_insert = ("""
 """)
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)
-                        ON CONFLICT (user_id) DO NOTHING
+                        ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
